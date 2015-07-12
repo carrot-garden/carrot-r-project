699fe2f3308c34c712602333bca93381d148f722
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2015-07-11 22:18:36 -0500

    name space

--- package/a/R/load.r
+++ package/a/R/load.r
@@ -10,7 +10,7 @@ library(devtools)
 #' 
 #' @details Function assumes that R working folder is inside git repository.
 #' 
-load = function(name, folder='package'){
+load.repo = function(name, folder='package'){
 	repo = git2r::repository('.', discover=TRUE)
 	work = git2r::workdir(repo)
 	devtools::load_all(file.path(work, folder, name), export_all=FALSE)
